80972c8ed6517a9717d58f6e817f11eb9d0b8b20
Focamacho <renancolombomail@gmail.com>
2021-04-14 03:23:46 -0300

    Fix netherite umbrellas not working with Curios

--- src/main/java/com/focamacho/vampiresneedumbrellas/handlers/ModObjects.java
+++ src/main/java/com/focamacho/vampiresneedumbrellas/handlers/ModObjects.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ModObjects {
+
 	public static List<Item> itemsList = new ArrayList<>();
 
 	public static Item umbrellaIron = null;
